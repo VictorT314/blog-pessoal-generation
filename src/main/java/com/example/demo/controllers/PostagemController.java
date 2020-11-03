d78db2d5361f65618b3fa3c1392afe75d7d81563
Victor Tavares <victor.as156@gmail.com>
2020-11-03 13:28:06 -0300

    Configurando findById

--- src/main/java/com/example/demo/controllers/PostagemController.java
+++ src/main/java/com/example/demo/controllers/PostagemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<PostagemModel>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PostagemModel> getById(@PathVariable Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 }
