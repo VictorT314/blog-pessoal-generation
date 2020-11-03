81fe08d0c3b60cee9bee7d9aeccbfdd4c6cbf64a
Victor Tavares <victor.as156@gmail.com>
2020-11-03 12:26:02 -0300

    Configurando repository

--- src/main/java/com/example/demo/repositories/PostagemRepository.java
+++ src/main/java/com/example/demo/repositories/PostagemRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.repositories;
 
-public class PostagemRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.PostagemModel;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<PostagemModel, Long>{
+	public List<PostagemModel> findAllByTituloContainingIgnoreCase(String titulo);
+		
 }
