254ab0bf3affe6afbac5133a70f130b4e7d436cb
Victor Tavares <victor.as156@gmail.com>
2020-11-03 13:07:07 -0300

    Configurando controller

--- src/main/java/com/example/demo/controllers/PostagemController.java
+++ src/main/java/com/example/demo/controllers/PostagemController.java
@@ -1,5 +1,27 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.PostagemModel;
+import com.example.demo.repositories.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
 public class PostagemController {
 
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<PostagemModel>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
 }
