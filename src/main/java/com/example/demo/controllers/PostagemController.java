98569170b2fc99e42d5dc9a216bdc8a12f1cfb98
Victor Tavares <victor.as156@gmail.com>
2020-11-04 15:01:18 -0300

    Configurando metodo put

--- src/main/java/com/example/demo/controllers/PostagemController.java
+++ src/main/java/com/example/demo/controllers/PostagemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +43,10 @@ public class PostagemController {
 	@PostMapping
 	public ResponseEntity<PostagemModel> post (@RequestBody PostagemModel postagem){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<PostagemModel> put (@RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 }
