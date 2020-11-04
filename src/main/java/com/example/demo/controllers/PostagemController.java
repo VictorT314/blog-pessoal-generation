b5ccd11058cf17791de197366cc26f0e3fff55af
Victor Tavares <victor.as156@gmail.com>
2020-11-04 14:51:39 -0300

    Configurando metodo post

--- src/main/java/com/example/demo/controllers/PostagemController.java
+++ src/main/java/com/example/demo/controllers/PostagemController.java
@@ -3,10 +3,13 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +39,8 @@ public class PostagemController {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@PostMapping
+	public ResponseEntity<PostagemModel> post (@RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
 }
