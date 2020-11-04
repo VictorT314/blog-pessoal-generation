2a27a1cdd72d944c57efc29694c018efce524e4e
Victor Tavares <victor.as156@gmail.com>
2020-11-04 20:33:40 -0300

    Configurando metodo delete

--- src/main/java/com/example/demo/controllers/PostagemController.java
+++ src/main/java/com/example/demo/controllers/PostagemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,10 @@ public class PostagemController {
 	@PutMapping
 	public ResponseEntity<PostagemModel> put (@RequestBody PostagemModel postagem){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
