39b9a7cb5227591cb3e955d819b39f87c35aa8b9
Victor Tavares <victor.as156@gmail.com>
2020-11-16 09:13:05 -0300

    Configurando UserDetails e UserDetailsService

---
+++ src/main/java/com/example/demo/seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.seguranca;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.repositories.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+		
+		return user.map(UserDetailsImpl::new).get();
+	}
+}
